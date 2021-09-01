1bfc98e0874457a4dd7b7052e0a8e2d3d631bcc2
jf <j@b.f>
2021-09-01 18:57:58 +0000

    initial library files

---
+++ source/ob/exposehtml/Elements.hx
@@ -0,0 +1,141 @@
+package ob.exposehtml;
+
+import js.html.Element;
+import js.html.HTMLDocument;
+import js.html.InputElement;
+
+class OuterElement {
+	var outer:Element;
+	var document:HTMLDocument;
+	var container:Element;
+
+	function new(document:HTMLDocument, ?outer:Element, labelText:String = "") {
+		this.outer = outer == null ? document.body : outer;
+		container = document.createDivElement();
+		container.classList.add("expose-item");
+		this.document = document;
+		var label = document.createDivElement();
+		label.classList.add("expose-item-label");
+		label.textContent = labelText;
+		container.appendChild(label);
+		this.outer.appendChild(container);
+	}
+}
+
+class ValueBoundElement<T> extends OuterElement {
+	var object:Dynamic;
+	var objectField:String;
+	var value:T;
+
+	public function new(object:Dynamic, objectField:String, document:HTMLDocument, ?outer:Element, label:String = "") {
+		super(document, outer, label);
+		this.object = object;
+		this.objectField = objectField;
+		if ((objectField != null) && (Reflect.getProperty(object, objectField) != null)) {
+			value = Reflect.getProperty(object, objectField);
+		}
+	}
+}
+
+class InputBoundElement<T> extends ValueBoundElement<T> {
+	var input:InputElement;
+	var func:T->Void;
+
+	public function new(object:Dynamic, objectField:String, document:HTMLDocument, ?outer:Element, label:String = "", func:T->Void) {
+		var labelText = '[${Type.getClassName(Type.getClass(object))}.${objectField}] ${label}';
+		super(object, objectField, document, outer, '${labelText}');
+		input = document.createInputElement();
+		this.func = func;
+		input.onchange = inputOnChange;
+		container.appendChild(input);
+		updateInputValue();
+	}
+
+	function updateInputValue() {
+		input.value = Std.string(value);
+	}
+
+	function inputOnChange() {
+		if (func != null) {
+			func(value);
+		}
+	}
+
+	function updateValue():Void {
+		if ((objectField != null) && (Reflect.getProperty(object, objectField) != null)) {
+			trace('set ${objectField}');
+			Reflect.setProperty(object, objectField, value);
+		}
+		var nextInputValue = Std.string(value);
+		if (input.value != nextInputValue) {
+			input.value = nextInputValue;
+		}
+	}
+}
+
+class NumericInputElement extends InputBoundElement<Float> {
+	var incrementStep:Float;
+
+	public function new(object:Dynamic, objectField:String, incrementStep:Float, document:HTMLDocument, ?outer:Element, ?label:String, func:Float->Void) {
+		super(object, objectField, document, outer, label, func);
+		input.type = "number";
+		this.incrementStep = incrementStep;
+		input.step = Std.string(incrementStep);
+	}
+
+	override function inputOnChange() {
+		var nextValue = Std.parseFloat(input.value);
+		if (Math.isNaN(nextValue)) {
+			return;
+		}
+		if (nextValue != value) {
+			value = nextValue;
+			updateValue();
+			super.inputOnChange();
+		}
+	}
+}
+
+class FunctionBoundElement extends OuterElement {
+	var func:Void->Void;
+
+	public function new(func:Void->Void, document:HTMLDocument, ?outer:Element, label:String = "") {
+		super(document, outer, label);
+		this.func = func;
+	}
+}
+
+class ButtonElement extends FunctionBoundElement {
+	var button:js.html.ButtonElement;
+
+	public function new(document:HTMLDocument, ?outer:Element, buttonText:String, label:String = "", func:Void->Void) {
+		super(func, document, outer, label);
+		button = this.document.createButtonElement();
+		button.textContent = buttonText;
+		button.onclick = () -> this.func();
+		container.append(button);
+	}
+}
+
+class CheckboxElement extends InputBoundElement<Bool> {
+	public function new(object:Dynamic, objectField:String, document:HTMLDocument, ?outer:Element, ?label:String, func:Bool->Void) {
+		super(object, objectField, document, outer, label, func);
+		input.type = "checkbox";
+	}
+
+	override function updateInputValue() {
+		input.checked = value;
+	}
+
+	override function inputOnChange() {
+		var nextValue = input.checked;
+		if (nextValue == null) {
+			return;
+		}
+		if (nextValue != value) {
+			value = nextValue;
+			updateValue();
+			super.inputOnChange();
+		}
+	}
+}
---
+++ source/ob/exposehtml/Expose.hx
@@ -0,0 +1,53 @@
+package ob.exposehtml;
+
+import js.html.Element;
+import js.html.HTMLDocument;
+import ob.exposehtml.Elements.ButtonElement;
+import ob.exposehtml.Elements.CheckboxElement;
+import ob.exposehtml.Elements.NumericInputElement;
+
+class Expose {
+	public var container(default, null):Element;
+
+	var document:HTMLDocument;
+
+	public function new(document:HTMLDocument, ?outer:Element) {
+		this.document = document;
+		var css = ".expose-container
+		{
+			display: flex;
+			justify-content: space-evenly;
+			flex-wrap: wrap;
+			margin-top:30px;
+		}
+		.expose-item-label { font-size: x-small; font-family: monospace; height:15px;}
+		.expose-item { margin: 5px; padding: 5px; border-radius:5px; background-color: #efefef;}
+		";
+		var style = this.document.createStyleElement();
+		style.textContent = css;
+		this.document.head.appendChild(style);
+		container = this.document.createDivElement();
+		container.classList.add("expose-container");
+		if (outer == null) {
+			this.document.body.appendChild(container);
+		} else {
+			outer.appendChild(container);
+		}
+	}
+
+	function containerOrDefault(?container:Element):Element {
+		return container == null ? this.container : container;
+	}
+
+	public function Numeric(object:Dynamic, objectField:String, incrementStep:Float, label:String = "", ?container:Element, func:Float->Void = null) {
+		new NumericInputElement(object, objectField, incrementStep, document, containerOrDefault(container), label, func);
+	}
+
+	public function Checkbox(object:Dynamic, objectField:String, label:String = "", ?container:Element, func:Bool->Void = null) {
+		new CheckboxElement(object, objectField, document, containerOrDefault(container), label, func);
+	}
+
+	public function Button(buttonText:String = "", label:String = "", ?container:Element, func:Void->Void) {
+		new ButtonElement(document, containerOrDefault(container), buttonText, label, func);
+	}
+}
